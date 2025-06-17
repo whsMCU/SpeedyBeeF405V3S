20eaa03b68626d4b873d400fd2c15cb24463064f
DESKTOP-NU36G62\WANG <wang8763@naver.com>
2025-06-17 22:21:19 +0900

    GUI : 데이터 로깅 버그 수정(Throttle) 및 로깅 데이터 추가(Arming_Flag, Flght_mode, Motor Data. etc.

--- SpeedyBeeF405V3S_GUI/SpeedyBeeF405V3S_GUI/Form1.cs
+++ SpeedyBeeF405V3S_GUI/SpeedyBeeF405V3S_GUI/Form1.cs
@@ -249,7 +249,7 @@ namespace SpeedyBeeF405V3S_GUI
                 writer.WriteLine(log);
                 Console.WriteLine(log); // 콘솔에도 출력
 
-                log = "DateTime, Roll, Pitch, Yaw, Alt, RollSetPoint, PitchSetPoint, Yaw SetPoint, Thorttle, yaw_heading_reference, altHold, lattitude, longitude, Sat_Num, gps_fix, Throttle_Hold_point, Alt_PID_Result, Debug[0], Debug[1], Debug[2], Debug[3]";
+                log = "DateTime, Arming_Flag, Flight_Mode, Roll, Pitch, Yaw, Alt, RollSetPoint, PitchSetPoint, Yaw SetPoint, Thorttle, yaw_heading_reference, altHold, lattitude, longitude, Sat_Num, gps_fix, Throttle_Hold_point, Alt_PID_Result, MOTOR[Right_Rear], MOTOR[Right_Front], MOTOR[Left_Rear], MOTOR[Left_Front], Debug[0], Debug[1], Debug[2], Debug[3]";
                 writer.WriteLine(log);
                 Console.WriteLine(log); // 콘솔에도 출력
             }
@@ -278,7 +278,7 @@ namespace SpeedyBeeF405V3S_GUI
                 data[1] /= 10;
                 data[4] /= 10;
                 data[5] /= 10;
-                string log = $"{DateTime.Now:HH:mm:ss.fff}, {data[0]}, {data[1]}, {data[2]}, {data[3]}, {data[4]}, {data[5]}, {data[6]}, {data[7]}, {data[41]}, {data[42]}, {data[8]}, {data[9]}, {data[43]}, {data[44]}, {data[45]}, {data[46]},{data[18]}, {data[19]}, {data[20]}, {data[21]}";
+                string log = $"{DateTime.Now:HH:mm:ss.fff}, {data[13]}, {data[11]}, {data[0]}, {data[1]}, {data[2]}, {data[3]}, {data[4]}, {data[5]}, {data[6]}, {data[7]}, {data[41]}, {data[42]}, {data[8]}, {data[9]}, {data[43]}, {data[44]}, {data[45]}, {data[46]}, {data[14]}, {data[15]}, {data[16]}, {data[17]}, {data[18]}, {data[19]}, {data[20]}, {data[21]}";
                 writer.WriteLine(log);
                 Console.WriteLine(log); // 콘솔에도 출력
             }
@@ -1941,7 +1941,7 @@ namespace SpeedyBeeF405V3S_GUI
             passed_data[4] = BitConverter.ToInt16(payload, 8) / 10;                  // RC_ROLL
             passed_data[5] = BitConverter.ToInt16(payload, 10) / 10;                 // RC_PITCH
             passed_data[6] = BitConverter.ToInt16(payload, 12);                      // RC_YAW
-            passed_data[7] = (BitConverter.ToInt16(payload, 14) -1000) / 10;         // RC_Throttole
+            passed_data[7] = BitConverter.ToInt16(payload, 14) / 10;                 // RC_Throttole
             passed_data[8] = BitConverter.ToInt32(payload, 16);     // posllh.lat
             passed_data[9] = BitConverter.ToInt32(payload, 20);     // posllh.lon
             passed_data[10] = BitConverter.ToInt16(payload, 24);    // batteryAverageCellVoltage
