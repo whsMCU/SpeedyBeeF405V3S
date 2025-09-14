59ba0d8b39e4808503022802247aa408cd4f0022
DESKTOP-NU36G62\WANG <wang8763@naver.com>
2025-09-14 23:42:29 +0900

    FC : LedStrip 수정중.

--- firmware/src/main/fc/init.c
+++ firmware/src/main/fc/init.c
@@ -268,15 +268,17 @@ void init(void)
     MSP_SET_MODE_RANGE(4, 33, 2, 1300, 2100); // SB : SURFACE
     MSP_SET_MODE_RANGE(5, 27, 4, 1700, 2100); // SD : FALISAFE
     MSP_SET_MODE_RANGE(6, 30, 5, 1700, 2100); // SF : HOME RESET
-    int i = 0;
-    uint32_t src = 0xFFFFFFFF;
-    ledConfig_t *ledConfig = &ledStripStatusModeConfig.ledConfigs[i];
+
+    uint32_t src = 0xFFD3FFFF;
+    ledConfig_t *ledConfig = &ledStripStatusModeConfig.ledConfigs[0];
     *ledConfig = src;
+    ledConfig_t *ledConfig1 = &ledStripStatusModeConfig.ledConfigs[1];
+    *ledConfig1 = src;
+    ledConfig_t *ledConfig2 = &ledStripStatusModeConfig.ledConfigs[2];
+    *ledConfig2 = src;
     reevaluateLedConfig();
 
     setModeColor(LED_AUX_CHANNEL, 0, 10);
-
-
 }
 
 void Param_Config_Init(void)
